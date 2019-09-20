308d94f52e2c71153bd6c4b5e79a5b4bd87228d3
7117 <qinglangsalaheiyou@163.com>
2019-09-20 23:37:02 +0800

    修改

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,6 +9,19 @@ class Index extends Controller
 {
     public function index()
     {
+//        http://tp.tp.tp/index/index/index?name=1111
+//        方法一：
+//        $request=Request::instance();
+//        $a=$request->get('name');
+//        方法二：
+//        $a=$this->request->get('name');
+//        第一个参数提示信息  第二个是网址路径
+        $this->success("aaa","http://www.baidu.com/");
         echo "1";
+    }
+
+    public function hello()
+    {
+        echo "2";
     }
 }
